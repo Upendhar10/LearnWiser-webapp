ac5d198880dacd1d22aa0cf4e5f938c821481a82
Upendhar <upendharnm.dev@gmail.com>
2026-01-14 15:24:23 +0530

    chore (frontend) : setup shadcn UI

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,11 @@
 import "./App.css";
+import { Button } from "@/components/ui/button";
 
 function App() {
   return (
-    <>
-      <h1>Frontend APP </h1>
-    </>
+    <div className="flex min-h-svh flex-col items-center justify-center">
+      <Button>Click me</Button>
+    </div>
   );
 }
 
